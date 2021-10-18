ca299da74b1efc5821df8754dcbe8ddd6044eb0f
Ruan Aragão <ruanaragao2@gmail.com>
2021-10-18 00:37:32 -0300

    Add hero

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Layout from "../components/layout/layout"
 import Container from "@mui/material/Container"
+import Hero from '../components/Hero'
 import Feature from "../components/Feature"
 import CustomerSupport from '../components/CustomerSupport'
 export default function Home() {
   return (
     <Layout>
       <Container maxWidth="lg">
+        <Hero />
         <Feature />
         <CustomerSupport />
       </Container>
