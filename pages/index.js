9acafedf9672936c1fab57f412a11aa8f064ebcb
Leslie Behum <llaurenb@gmail.com>
2021-10-21 11:24:56 -0400

    move Form compononet to resources.js from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Layout from "../components/layout/layout"
 import Container from "@mui/material/Container"
 import Feature from "../components/Feature"
-import Form from "../components/Form"
 import CustomerSupport from "../components/CustomerSupport"
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
       <Container maxWidth="lg">
         <Feature />
         <CustomerSupport />
-        <Form />
       </Container>
     </Layout>
   )
