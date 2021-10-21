b4be3e5db8e74992054b0fcd6e82cb8b17273ad9
Leslie Behum <llaurenb@gmail.com>
2021-10-20 20:34:53 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Layout from "../components/layout/layout"
 import Container from "@mui/material/Container"
 import Feature from "../components/Feature"
 import Form from "../components/Form"
-import CustomerSupport from '../components/CustomerSupport'
+import CustomerSupport from "../components/CustomerSupport"
+
 export default function Home() {
   return (
     <Layout>
