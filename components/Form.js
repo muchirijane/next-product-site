eb2ae855551a8e936b6515ab4a1ea338244ef2c4
Leslie Behum <llaurenb@gmail.com>
2021-10-20 20:25:00 -0400

    finish styling Form.js components

--- components/Form.js
+++ components/Form.js
@@ -3,46 +3,64 @@ import Container from "@mui/material/Container"
 import Button from "@mui/material/Button"
 import Grid from "@mui/material/Grid"
 import TextField from "@mui/material/TextField"
-import { green } from "@mui/material/colors"
-
-
 
 const Form = () => {
+    
     return (
-        <Container maxWidth='sm'>
-            <Grid direction="column" container spacing={3} mt={2} justify="center">
-                <Grid
-                item
-                >
-                <TextField
+        <Container maxWidth='md'>
+            <Grid
                 sx={{
-                "& .MuiFilledInput-root": { borderRadius: '50px' }
+                    p: '20px',
+                    borderRadius: '10px',
+                    border: '2px solid lightgrey',
+                    "@media (max-width: 720px)": {
+                        border: 'none'
+                    }
                 }}
+                direction="column"
+                container
+                mt={2}
+                justify="center"
+                alignItems="center">
+                <h1>Send a Message</h1>    
+                <TextField
+                    sx={{
+                    mb: '30px',
+                    "& .MuiFilledInput-root": { borderRadius: '50px' },
+                    "& .MuiFilledInput-root::before": { borderBottom: 0 },
+                    "& .MuiFilledInput-root::after": { borderBottom: 0 },
+                        "& .MuiFilledInput-root:hover:not(.Mui-disabled):before": { borderBottom: 0 },
+                    "& .MuiFilledInput-input": { ml: '15px' }
+                    }}
                 fullWidth
                 hiddenLabel
                 id="filled-hidden-label-normal"
                 defaultValue="Your Name"
                 variant="filled"
                     />  
-                </Grid>
-                <Grid
-                item
-                >
                 <TextField
-                sx={{
-                "& .MuiFilledInput-root": { borderRadius: '50px' }
-                }}
+                    sx={{
+                    mb: '30px',
+                    "& .MuiFilledInput-root": { borderRadius: '50px' },
+                    "& .MuiFilledInput-root::before": { borderBottom: 0 },
+                    "& .MuiFilledInput-root::after": { borderBottom: 0 },
+                    "& .MuiFilledInput-root:hover:not(.Mui-disabled):before": { borderBottom: 0 },
+                    "& .MuiFilledInput-input": { ml: '15px' }
+                    }}
                 fullWidth
                 hiddenLabel
                 id="filled-hidden-label-normal"
                 defaultValue="Your Email"
                 variant="filled"
                     />
-                </Grid>
-                <Grid
-                item
-                >
                 <TextField
+                    sx={{
+                    mb: '30px',
+                    "& .MuiFilledInput-root::before": { borderBottom: 0 },
+                    "& .MuiFilledInput-root::after": { borderBottom: 0 },
+                    "& .MuiFilledInput-root:hover:not(.Mui-disabled):before": { borderBottom: 0 },
+                     "& .MuiFilledInput-input": { m: '10px' }
+                    }}
                 fullWidth
                 hiddenLabel
                 multiline
@@ -51,17 +69,23 @@ const Form = () => {
                 defaultValue="Your Message"
                 variant="filled"
                 />
-                </Grid>
                 <Button
                     sx={{
-                        borderRadius: '20px',
-                        color: '#FFF',
-                        backgroundColor: '#02897A',
-                        alignSelf: 'flex-start',
-                        m: '20px'
+                    p: '15px',
+                    borderRadius: '50px',
+                    color: '#FFF',
+                    backgroundColor: '#02897A',
+                    alignSelf: 'flex-start',
+                    textTransform: 'none',
+                    minWidth: '150px',
+                    boxShadow: '2px 3px 5px lightgrey',
+                        "&:hover": {
+                            boxShadow: '2px 3px 5px lightgrey',
+                            bgcolor: 'rgb(1, 95, 85)'
+                        }
                     }}
                 >Send</Button>
-            </Grid>   
+            </Grid> 
         </Container>
     )
 }
