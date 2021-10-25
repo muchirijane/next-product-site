a82d2c2679dfd9f53daded4ec8f3b1c5313f5e76
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2021-10-25 14:50:20 +0100

    Add Stories component under Resources page

--- pages/resources.js
+++ pages/resources.js
@@ -1,12 +1,14 @@
 import Layout from "../components/layout/layout"
 import Container from "@mui/material/Container"
 import Form from "../components/Form"
+import Stories from "../components/Stories"
 
 export default function Resources() {
   return (
     <Layout>
       <Container maxWidth="lg">
         <h1>Resources Page</h1>
+        <Stories/>
         <Form />
       </Container>
     </Layout>
